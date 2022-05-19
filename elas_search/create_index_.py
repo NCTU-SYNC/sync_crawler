7a484eb3a3f3be1c4cf282def0ae0253493c7e91
timmyhorng <timmyhorng@gmail.com>
2022-05-20 00:40:53 +0800

    [BE]Migrate es version from 8 to 7(more easy to setup)

--- elas_search/create_index_.py
+++ elas_search/create_index_.py
@@ -5,43 +5,39 @@ from elasticsearch import Elasticsearch
 database_config = ConfigParser(interpolation=None)
 database_config.read('..\database_config.ini')
 
-MONGODB_URI_LOCAL = database_config['cache']['uri']
+username = database_config['elastic']['username']
+secret   = database_config['elastic']['secret']
+local_secret = database_config['elastic']['local_secret']
+
+es = Elasticsearch(f"http://{username}:{local_secret}@localhost:9200")
+# es = Elasticsearch(f"https://{username}:{secret}@212b-140-113-214-146.jp.ngrok.io")
+
+MONGODB_URI = database_config['cache']['uri']
+
 LOCAL_DATABASE    = database_config['cache']['db']
 LOCAL_COLLECTION  = database_config['cache']['collection']
 
-username = database_config['elastic']['username']
-secret   = database_config['elastic']['secret']
-es = Elasticsearch(
-    ['https://localhost:9200/'],
-    basic_auth=(username,secret),
-)
 
-
-
-# MONGODB_URI_MAIN  = database_config['main']['uri']
-# MAIN_DATABASE     = database_config['main']['db']
-# MAIN_COLLECTION   = database_config['main']['collection']
+MAIN_DATABASE     = database_config['main']['db']
+MAIN_COLLECTION   = database_config['main']['collection']
 # DEV_MAIN_DATABASE = database_config['dev-main']['db']
 # DEV_MAIN_COLLECTION   = database_config['dev-main']['collection']
-client = MongoClient(MONGODB_URI_LOCAL)
-db  = client[LOCAL_DATABASE]
-col = db[LOCAL_COLLECTION]
 
-offset = 0
-limitNum  = 15
-Newses=col.find().skip(offset).limit(limitNum)
+def index_DB_data(_db_uri,_db,_col,offset,limitNum,index,doc_type):
+    client = MongoClient(_db_uri)
+    db  = client[_db]
+    col = db[_col]
+    Newses=col.find().skip(offset).limit(limitNum)
 
-
-
-for news in Newses:
-    print(news['title'],type(news['_id']),news['_id'])
-    doc = {
-        'title'  : news['title'],
-        'content': news['content']
-    }
-    es.index(index="news",id=news['_id'],document=doc)
-# es.indices.refresh(index="news")   
-# resp = es.get(index="news", id=1)
-# print(resp['_source'])
+    for news in Newses:
+        print(news['title'],type(news['_id']),news['_id'])
+        doc = {
+            'title'  : news['title'],
+            'content': news['content']
+        }
+        es.index(index=index,doc_type=doc_type,id=news['_id'],body=doc)
 
 
+
+# index_DB_data(MONGODB_URI,LOCAL_DATABASE,LOCAL_COLLECTION,0,15,"cache","cacheType")  #345
+index_DB_data(MONGODB_URI,MAIN_DATABASE,MAIN_COLLECTION ,0,100000,"main","mainType")    #1000,000
--- elas_search/search_file.py
+++ elas_search/search_file.py
@@ -1,32 +1,27 @@
+from configparser import ConfigParser
 from elasticsearch import Elasticsearch
 
-# Elastic search configuation
-es = Elasticsearch("http://localhost:9200")
+database_config = ConfigParser(interpolation=None)
+database_config.read('..\database_config.ini')
 
-# es.indices.refresh(index="news")
-query={
-    "match_all": {}
-}
-resp = es.search(index="news", query=query)
-limit=100
-for index,hit in enumerate(resp['hits']['hits']):
-    if limit == index:
-        break
-    print(hit["_source"]["title"])
-    print((hit["_score"]))
-    print(hit["_source"]["content"],end="\n\n")
+username = database_config['elastic']['username']
+secret   = database_config['elastic']['secret']
+local_secret = database_config['elastic']['local_secret']
+
+es = Elasticsearch(f"http://{username}:{local_secret}@localhost:9200")
+# es = Elasticsearch(f"https://{username}:{secret}@212b-140-113-214-146.jp.ngrok.io")
 
 
-
-
-
-query = {
-    "match": {
-        "content":"能源 電動車 "
+body={
+    "from":0,
+    "size":10000,   #可以試試看10001
+    "query": {
+        "match": {
+            "content":"酷喔"   #doc_type  #能源 電動車 
+        }
     }
 }
-
-resp = es.search(index="news", query=query)
+resp = es.search(index="main", body=body)
 # print(resp)
 print(f"Got {resp['hits']['total']['value']} Hits:")
 limit=5
@@ -35,5 +30,9 @@ for index,hit in enumerate(resp['hits']['hits']):
         break
     print(hit["_source"]["title"])
     print((hit["_score"]))
-    print(hit["_source"]["content"],end="\n\n")
+    # print(hit["_source"]["content"],end="\n\n")
+
+
+
+
     
